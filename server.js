a05ce733f0b591744003eef6a3c3b18d27b01b44
Aline Gimenez de Castro <alinegimenezdecastro@hotmail.com>
2021-07-24 01:09:30 +0000

    Create tag (/createtag -> post) e read credentials (/ -> get)

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.post('/createtag', async function (request, response) {
     const uuid1 = uuid();
     const tag_number = request.body.tag_number;
     const user_name = request.body.user_name;
-    const resposta = createTag(uuid1, tag_number, user_name);
+    const resposta = await createTag(uuid1, tag_number, user_name);
     response.status(200).send();
 })
 
