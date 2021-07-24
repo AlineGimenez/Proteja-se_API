313e49c80158235bf256b18f4cf35c495ba79b8b
Aline Gimenez de Castro <alinegimenezdecastro@hotmail.com>
2021-07-24 00:21:31 +0000

    Primeiros ajustes - antes do Heroku

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express');
-
+const express = require('express');  // import express;
 const server = express();
+const cors = require('cors');
+const { uuid } = require('uuidv4');
 
-server.get('/', function(request, response) {
-    response.send('Hello World!');
+server.use(cors())
+server.use(express.json())
+
+server.get('/', async function (request, response) {
+    response.json("HELLO WORLD");
 })
 
 server.listen(process.env.PORT || 3000);
